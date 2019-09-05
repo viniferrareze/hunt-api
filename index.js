05fd23ef6e0963f07a6606a1e01e3da94ceb3a52
carlos vinícius ferrareze <viniferrareze@gmail.com>
2019-09-05 00:02:42 -0300

    Iniciando CRUD

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const requireDir = require("require-dir");
 
+
+//iniciando o app
 const app = express();
 
+//iniciando a conexao com DB
+mongoose.connect("mongodb://localhost:27017/hunt", { useNewUrlParser: true })
+requireDir("./src/models");
+
+const Product = mongoose.model('Product');
+
+//criando a primeira rota
 app.get('/', (req, res) => {
-    res.send("Olá mundo");
+    Product.create({
+        title: 'React',
+        description: 'Teste',
+        url: 'www.google.com'
+    });
+
+    return res.send("Olá mundo");
 });
 
-app.listen(3001, ()=> {
+//iniciando o servidor
+app.listen(3001, () => {
     console.log("servidor ouvindo...");
 });
